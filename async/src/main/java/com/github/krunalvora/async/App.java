8fd792a8f9618d085e6ba0d4b4d8b24a166109ed
Krunal Vora <krunalvora4@gmail.com>
2021-06-19 16:26:33 +0530

    gitignore changes and async project structure changes

---
+++ async/src/main/java/com/github/krunalvora/async/App.java
@@ -0,0 +1,16 @@
+package com.github.krunalvora.async;
+
+public class App {
+
+  public static void main(String[] args) {
+
+    System.out.println("Hello World!");
+
+    // MyTask myTask = new MyTask();
+    // myTask.run();
+
+    // Runnable lambdaRunnableTask = () -> {
+    //   System.out.println("Running lambdaRunnableTask ...");
+    // };
+  }
+}
