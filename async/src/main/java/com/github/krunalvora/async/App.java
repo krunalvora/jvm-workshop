590328e82239b19da2a95f094a5cfe46e7044a04
Krunal Vora <krunalvora4@gmail.com>
2021-06-19 16:59:03 +0530

    async initial files

--- async/src/main/java/com/github/krunalvora/async/App.java
+++ async/src/main/java/com/github/krunalvora/async/App.java
@@ -1,16 +1,52 @@
 package com.github.krunalvora.async;
 
+import java.util.concurrent.Callable;
+
 public class App {
 
   public static void main(String[] args) {
 
-    System.out.println("Hello World!");
+    // Runnables
+    MyTask myTask = new MyTask();
+    myTask.run();
 
-    // MyTask myTask = new MyTask();
-    // myTask.run();
 
-    // Runnable lambdaRunnableTask = () -> {
-    //   System.out.println("Running lambdaRunnableTask ...");
-    // };
+    Runnable myRunnableTask = new Runnable() {
+      @Override
+      public void run() {
+        System.out.println("Running myRunnableTask ...");
+      }
+    };
+    myRunnableTask.run();
+
+
+    Runnable myLambdaRunnableTask = () -> {
+      System.out.println("Running myLambdaRunnableTask ...");
+    };
+    myLambdaRunnableTask.run();
+
+
+    // Callables
+    Callable<String> myCallableTask = new Callable<String>() {
+      @Override
+      public String call() throws Exception {
+        return "Running myCallableTask ...";
+      }
+    };
+    try {
+      myCallableTask.call();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    Callable<String> myLambdaCallableTask = () -> "Running myLambdaCallableTask ...";
+    try {
+      myLambdaCallableTask.call();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+
+
   }
 }
--- async/src/main/java/com/github/krunalvora/async/MyTask.java
+++ async/src/main/java/com/github/krunalvora/async/MyTask.java
@@ -1,7 +1,6 @@
 package com.github.krunalvora.async;
 
 public class MyTask implements Runnable{
-
   @Override
   public void run() {
     System.out.println("Running MyTask ...");
