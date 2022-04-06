41da91e323a1717d26a5388f7c43a2db7ce8166a
Krunal Vora <krunalvora4@gmail.com>
2022-04-06 06:41:22 +0000

    java practice snippets

---
+++ practice/src/main/java/com/github/krunalvora/snippets/Arrays2D.java
@@ -0,0 +1,18 @@
+package com.github.krunalvora.snippets;
+
+import java.util.Arrays;
+
+public class Arrays2D {
+
+    public static void main(String[] args) {
+        
+        int[][] arr = new int[][]{{4, 5, 6}, {1, 2, 3}, {7, 8, 9}};
+
+        System.out.println(Arrays.deepToString(arr));
+
+
+
+    }
+    
+    
+}
