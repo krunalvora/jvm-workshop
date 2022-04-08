05ca79b11b7f2d0044586d4f6da26f8ba1b1073e
Krunal Vora <krunalvora4@gmail.com>
2022-04-08 01:33:10 +0000

    heaps

---
+++ practice/src/main/java/com/github/krunalvora/snippets/Heaps.java
@@ -0,0 +1,32 @@
+package com.github.krunalvora.snippets;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Heaps {
+
+    public static void main(String[] args) {
+        
+        // Create min heap in Java
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        minHeap.add(1);
+        minHeap.add(4);
+        minHeap.add(100);
+
+        System.out.println(minHeap.toString());
+
+
+        // Create max heap in Java
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        maxHeap.add(1);
+        maxHeap.add(4);
+        maxHeap.add(100);
+
+        System.out.println(maxHeap.toString());
+
+    }
+
+
+}
