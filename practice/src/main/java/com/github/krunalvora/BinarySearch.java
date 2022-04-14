5f393c84d4cad13200f5b8e74695489fb09025c8
Krunal Vora <krunalvora4@gmail.com>
2022-04-14 08:19:17 +0000

    junit jupiter tests

--- practice/src/main/java/com/github/krunalvora/BinarySearch.java
+++ practice/src/main/java/com/github/krunalvora/BinarySearch.java
@@ -13,13 +13,16 @@ public class BinarySearch {
     }
 
     public static boolean contains(int[] nums, int target) {
+
+        if (nums == null) return false;
+
         int left = 0;
         int right = nums.length - 1;
 
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) return true;
-            else if (nums[mid] > target) right = mid;
+            else if (nums[mid] > target) right = mid - 1;
             else if (nums[mid] < target) left = mid + 1;
         }
         return false;
--- practice/src/test/java/com/github/krunalvora/BinarySearchTest.java
+++ practice/src/test/java/com/github/krunalvora/BinarySearchTest.java
@@ -1,14 +1,17 @@
 package com.github.krunalvora;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class BinarySearchTest {
 
     @Test
     public void testContains() {
-        assertEquals(true, true);
-    }
-    
+        assertEquals(true, BinarySearch.contains(new int[]{1, 2, 3, 4, 5}, 4));
+        assertEquals(false, BinarySearch.contains(new int[]{1, 2, 3, 4, 5}, 0));
+        assertEquals(false, BinarySearch.contains(new int[]{}, 0));
+        assertEquals(false, BinarySearch.contains(null, 0));
+        assertEquals(true, BinarySearch.contains(new int[3], 0));
+        assertEquals(false, BinarySearch.contains(new int[3], 1));
+    }    
 }
