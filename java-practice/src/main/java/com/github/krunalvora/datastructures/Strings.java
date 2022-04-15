df1a83dbec6cfac72a34578c2ef20f5e46172932
Krunal Vora <krunalvora4@gmail.com>
2022-04-15 21:15:10 +0000

    stringbuilder

---
+++ java-practice/src/main/java/com/github/krunalvora/datastructures/Strings.java
@@ -0,0 +1,18 @@
+package com.github.krunalvora.datastructures;
+
+public class Strings {
+
+    public static void main(String[] args) {
+        
+        StringBuilder sb = new StringBuilder();
+
+        sb.append('a');
+        sb.append('s');
+        sb.append('-');
+
+        System.out.println(sb.toString());
+
+
+    }
+    
+}
