591ff25613c04c7f27d1d3cd6af0bc6a1dd1c92a
Krunal Vora <krunalvora4@gmail.com>
2022-05-08 20:29:28 +0000

    dijkstra incomplete

--- java-practice/src/main/java/com/github/krunalvora/datastructures/graphs/Dijkstra.java
+++ java-practice/src/main/java/com/github/krunalvora/datastructures/graphs/Dijkstra.java
@@ -1,6 +1,15 @@
 package com.github.krunalvora.datastructures.graphs;
 
+import java.util.*;
+import javafx.util.Pair;
+
 public class Dijkstra {
+
+    PriorityQueue<Pair<Integer, Node>> minHeap;
+    int numNodes;
+    boolean[] visited;
+    List<Node> previous;
+    int[] bestDistance;
 
     public static void main(String[] args) {
         DirectedWeightedGraph graph = new DirectedWeightedGraph();
@@ -24,6 +33,17 @@ public class Dijkstra {
         graph.print();
 
         System.out.println(graph.getWeight(A, C));
+    }
+
+    public Dijkstra (int numNodes) {
+        // TODO
+        minHeap = new PriorityQueue<>();
+        this.numNodes = numNodes;
+        visited = new boolean[numNodes];
+        previous = new ArrayList<>(numNodes);
+        bestDistance = new int[numNodes];
+
+
     }
 
     public static void dijkstra(DirectedGraph g, int source) {
