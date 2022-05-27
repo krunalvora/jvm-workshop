da2419d001be1a7d3dfd9a3f67215b7a2cc95902
Krunal Vora <krunalvora4@gmail.com>
2022-05-27 08:50:04 +0000

    subset sums memoization

--- java-practice/src/main/java/com/github/krunalvora/algorithms/dynamicprogramming/SubsetSum.java
+++ java-practice/src/main/java/com/github/krunalvora/algorithms/dynamicprogramming/SubsetSum.java
@@ -1,5 +1,8 @@
 package com.github.krunalvora.algorithms.dynamicprogramming;
 
+import java.util.*;
+import javafx.util.*;
+
 /**
  * Given an array of strictly positive integers arr and a strictly positive integer k,
  * count the number of subsets that sum to k.
@@ -15,25 +18,34 @@ package com.github.krunalvora.algorithms.dynamicprogramming;
 
 
 public class SubsetSum {
-    public static void main(String[] args) {
-        int[] arr = new int[]{1, 2, 3, 1, 4};
-        int subsetsCount = countSubSets(arr, 6);
-        System.out.println(subsetsCount);
-    }
+
+    static Map<Pair<Integer, Integer>, Integer> memo;
 
     public static int countSubSets(int[] arr, int k) {
+        memo = new HashMap<>();
         int output = rec(arr, 0, k);
         return output;
     }
 
     public static int rec(int[] arr, int i, int k) {
+        Pair<Integer, Integer> pair = new Pair<>(i, k);
+        if (memo.containsKey(pair)) {
+            return memo.get(pair);
+        }
+
         if (k == 0) return 1;
 
         if ( k < 0 || i == arr.length) return 0;
 
-        return rec(arr, i + 1, k) + 
-               rec(arr, i + 1, k - arr[i]);
+        memo.put(pair, rec(arr, i + 1, k) + rec(arr, i + 1, k - arr[i]));
+
+        return memo.get(pair);
     }
 
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 1, 4};
+        int subsetsCount = countSubSets(arr, 6);
+        System.out.println(subsetsCount);
+    }
 
 }
